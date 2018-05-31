deea450ba9b4a7ebfa9ab9d89a55184d4c908483
genglang <657578302@qq.com>
2018-06-01 00:22:18 +0800

    'create'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LoginModule} from './login/login.module';
+import {SharedModule} from './shared/shared.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        SharedModule,
+        LoginModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
